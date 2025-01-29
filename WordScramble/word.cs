1b054c09a263789828382507f00241544fb66899
Olga <omfe1811@gmail.com>
2025-01-29 11:54:20 +0100

    commented out assignment useful snippets

--- WordScramble/word.cs
+++ WordScramble/word.cs
@@ -2,20 +2,12 @@ namespace WordScramble
 {
     public class RandomWord
     {
-        public List<string> RandomWordList { get; set; } = new List<string>();
 
         public RandomWord() { }
 
 
-        //populate our randomwordlist
-
-        public void PopulateList(string word)
-        {
-            RandomWordList.Add(word);
-        }
-
-        // Method to pick a random word from RandomWordList
-        public string PickWord()
+        // Method to get a random word from user input
+        /* public string PickWord()
         {
             Random random = new Random();
             int listLength = RandomWordList.Count;
@@ -23,14 +15,15 @@ namespace WordScramble
             string secretWord = RandomWordList[randomIndex];
             return secretWord;
         }
-        public static string GetRandomWordFromStaticList()
-        {
-            string[] predefinedWords = { "Oscar", "dwight", "Michael", "Angela" };
-            Random random = new Random();
-            int randomIndex = random.Next(predefinedWords.Length);
-            System.Console.WriteLine("index no" + randomIndex);
-            return predefinedWords[randomIndex];
+ */
+        /*  public static string GetRandomWordFromStaticList()
+         {
+             string[] predefinedWords = { "Oscar", "dwight", "Michael", "Angela" };
+             Random random = new Random();
+             int randomIndex = random.Next(predefinedWords.Length);
+             System.Console.WriteLine("index no" + randomIndex);
+             return predefinedWords[randomIndex];
 
-        }
+         } */
     }
 }
